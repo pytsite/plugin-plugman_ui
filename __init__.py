c5cae79c3afb002f54b88a8562568c822e4b50e8
Alexander Shepetko <a@shepetko.com>
2017-12-02 10:41:33 +0200

    Release 1.1

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ __license__ = 'MIT'
 
 
 def _init():
-    from pytsite import plugman, setup, update, console
+    from pytsite import plugman
     from plugins import assetman, permissions, settings, http_api
     from . import _settings_form, _http_api_controllers
 
@@ -16,11 +16,11 @@ def _init():
     assetman.t_js(__name__ + '@**')
 
     # HTTP API
-    http_api.handle('POST', 'plugman/install/<name>', _http_api_controllers.PostInstall(),
+    http_api.handle('POST', 'plugman/install/<name>', _http_api_controllers.PostInstall,
                     'plugman_ui@post_install')
-    http_api.handle('POST', 'plugman/uninstall/<name>', _http_api_controllers.PostUninstall(),
+    http_api.handle('POST', 'plugman/uninstall/<name>', _http_api_controllers.PostUninstall,
                     'plugman_ui@post_uninstall')
-    http_api.handle('POST', 'plugman/upgrade/<name>', _http_api_controllers.PostUpgrade(),
+    http_api.handle('POST', 'plugman/upgrade/<name>', _http_api_controllers.PostUpgrade,
                     'plugman_ui@post_upgrade')
 
     if not plugman.is_dev_mode():
@@ -29,10 +29,6 @@ def _init():
 
         # Settings
         settings.define('plugman', _settings_form.Form, 'plugman_ui@plugins', 'fa fa-plug', 'plugman_ui@manage')
-
-        # Event handlers
-        setup.on_setup(lambda: console.run_command('plugman:install', {'reload': False}), 999)
-        update.on_update_after(lambda: console.run_command('plugman:update', {'reload': False}))
 
 
 _init()
